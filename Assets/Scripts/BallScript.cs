54c8e1e4ad0fce2af88b11f6d4a03fa92ab20d53
KURAMA\Kai <kleber_rodrigo89@hotmail.com>
2018-02-22 18:22:52 -0300

    Goal completed

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallScript : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		MoveBall ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.FindWithTag ("Brick") == null) {
+			SceneManager.LoadScene ("Start");
+		}
+	}
+
+	void MoveBall () {
+		float rand = Random.Range (0, 2);
+		if (rand > 0) {
+			rb.AddForce (new Vector2 (20, 15), ForceMode2D.Force);
+		} else {
+			rb.AddForce (new Vector2 (-20, 15), ForceMode2D.Force);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		if (c.gameObject.tag == "Brick") {
+			Destroy (c.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D c) {
+		SceneManager.LoadScene ("Start");
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float velocidade;
+
+	void FixedUpdate () {
+		float move_x = Input.GetAxisRaw ("Horizontal");
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move_x, 0) * velocidade;
+	}
+}
